0709c3d28f63c1bcdf2bb1917fc0ab2fbe6ff18f
Alex Pawlowski <apawlows@vols.utk.edu>
2016-04-10 01:41:24 -0400

    Addressed comments to change poster
    Also split up parts of the poster to external svgs to import since Inkscape was bogging down on the size.

--- plot-data.R
+++ plot-data.R
@@ -6,20 +6,20 @@ library(ggplot2)
 # stemfacultyplot
 
 ### stem men women by department ----
-stemfacultyplot <- ggplot(data = stemfacultynums, aes(x = reorder(Department, freq), y = freq, group = Gender, fill = Gender, colour = Gender))+
-  scale_colour_manual(values = c("#754a7e","#2197A9")) +
+stemfacultyplot <- ggplot(data = stemfacultynums, aes(x = Department, y = freq, group = Gender, fill = Gender, colour = Gender))+
+  scale_colour_manual(values = c("#754a7e","#2197A9")) +  #reorder(Department, freq)
   scale_fill_manual(values = c("#754a7e","#2197A9")) +
   #scale_colour_brewer(palette = "Set2") +
   #scale_fill_brewer(palette = "Set2") +
   #geom_smooth(alpha = 0.6, size = 1.5)+
-  geom_line(size = 3.5, alpha = 0.8) +
+  #geom_line(size = 3.5, alpha = 0.8) +
   geom_point(size = 7, shape = 21, colour = "white") +
   xlab("Departments") +
   ylab("Faculty Members") +
   ggtitle("2015-2016 STEM Faculty by Gender at Tennessee-Knoxville") +
     theme(
       axis.text.y = element_text(color="#58595B", size = 20, vjust = 0.5),
-      axis.text.x = element_text(color="#58595B", size = 10, vjust = 0.5, hjust = 1, angle = 70),
+      axis.text.x = element_text(color="#58595B", size = 15, vjust = 0.5, hjust = 1, angle = 70),
       axis.title.y = element_text(color="#58595B", face = "bold", size = 20, vjust = 0.5, angle = 90),
       axis.title.x = element_text(color="#58595B", face = "bold", size = 20, vjust = 0.5),
       axis.ticks.y = element_line(color="#58595B", size = 2),
@@ -33,9 +33,9 @@ stemfacultyplot <- ggplot(data = stemfacultynums, aes(x = reorder(Department, fr
   )
 
 stemfacultyplot
-ggsave(filename = "plots/stemfacultybydepartmentplot-norange.svg", plot = stemfacultyplot, width = 8, height = 6, units = "in")
+ggsave(filename = "plots/stemfacultybydepartmentplot-norange1.svg", plot = stemfacultyplot, width = 8, height = 6, units = "in")
 
-### men women non-stem stem aggregate ----
+### men women non-stem stem faculty by gender aggregate ----
 facultybygenderplot <- ggplot(data = facultybygender, aes(x = Gender, y = freq, group = Designation, fill = Gender, colour = Gender))+
   scale_colour_manual(values = c("#754a7e","#2197A9")) +
   scale_fill_manual(values = c("#754a7e","#2197A9")) +
@@ -69,8 +69,43 @@ facultybygenderplot <- ggplot(data = facultybygender, aes(x = Gender, y = freq, 
 facultybygenderplot
 ggsave(filename = "plots/facultybygenderplot.svg", plot = facultybygenderplot, width = 8, height = 6, units = "in")
 
+### men women non-stem stem faculty by gender aggregate ----
+facultytenurebygenderplot <- ggplot(data = facultytenurebygender, aes(x = Gender, y = freq, group = Designation, fill = Gender, colour = Gender))+
+  scale_colour_manual(values = c("#754a7e","#2197A9")) +
+  scale_fill_manual(values = c("#754a7e","#2197A9")) +
+  #scale_colour_brewer(palette = "Set2") +
+  #scale_fill_brewer(palette = "Set2") +
+  geom_bar(colour = "#58595b", stat = "identity") +
+  facet_grid(.~Designation)+
+  xlab("Gender") +
+  ylab("Faculty Members") +
+  ggtitle("Non-STEM and STEM Tenured or Tenure Track Faculty at Tennessee-Knoxville") +
+  theme(
+    axis.text.y = element_text(color="#58595B", size = 20, vjust = 0.5),
+    axis.text.x = element_text(color="#58595B", size = 20, vjust = 0.5),
+    axis.title.y = element_text(color="#58595B", face = "bold", size = 20, vjust = 0.5, angle = 90),
+    axis.title.x = element_text(color="#58595B", face = "bold", size = 20, vjust = 0.5),
+    axis.ticks.y = element_line(color="#58595B", size = 2),
+    axis.ticks.x = element_line(color="#58595B", size = 2),
+    
+    #strip == facet
+    strip.background = element_rect(colour = "#58595B", fill = "#58595B"),
+    strip.text = element_text(color="#ffffff", size = 20),
+    
+    plot.title = element_text(color="#58595B", face = "bold", size = 20, hjust = 0.5, vjust = 0),
+    panel.background = element_blank(),
+    #panel.border = element_rect(color="#58595B"),#element_blank(),
+    panel.grid.major.x = element_line(color = "#A7A9AC", size = 0.5),
+    panel.grid.major.y = element_line(color = "#A7A9AC", size = 0.5),
+    legend.position = "right"
+  )
+
+facultytenurebygenderplot
+ggsave(filename = "plots/facultytenurebygenderplot.svg", plot = facultytenurebygenderplot, width = 8, height = 6, units = "in")
+
+
 ### men women non-stem stem by position ----
-facultybyposition$Position_f = factor(facultybyposition$Position, levels = c('Professor','Lecturer','Assoc.Prof','Asst.Prof','Instructor','Other'))
+facultybyposition$Position_f = factor(facultybyposition$Position, levels = c('Non-Tenure','Asst.Prof','Assoc.Prof','Professor'))
 facultybypositionplot <- ggplot(data = facultybyposition, aes(x = Gender, y = freq, group = Designation, fill = Gender, colour = Gender))+
   scale_colour_manual(values = c("#754a7e","#2197A9")) +
   scale_fill_manual(values = c("#754a7e","#2197A9")) +
@@ -96,13 +131,13 @@ facultybypositionplot <- ggplot(data = facultybyposition, aes(x = Gender, y = fr
     plot.title = element_text(color="#58595B", face = "bold", size = 20, hjust = 0.5, vjust = 0),
     panel.background = element_blank(),
     #panel.border = element_rect(color="#58595B"),#element_blank(),
-    panel.grid.major.x = element_line(color = "#A7A9AC", size = 0.5),
+    panel.grid.major.x = element_blank(),
     panel.grid.major.y = element_line(color = "#A7A9AC", size = 0.5),
     legend.position = "right"
   )
 
 facultybypositionplot
-ggsave(filename = "plots/facultybypositionplot.svg", plot = facultybypositionplot, width = 14, height = 6, units = "in")
+ggsave(filename = "plots/facultybypositionplot.svg", plot = facultybypositionplot, width = 10, height = 6, units = "in")
 
 ### men women aggregate by position ----
 facultyaggregatebypositionplot <- ggplot(data = facultybyposition, aes(x = Gender, y = freq, fill = Gender, colour = Gender))+
@@ -110,10 +145,10 @@ facultyaggregatebypositionplot <- ggplot(data = facultybyposition, aes(x = Gende
   scale_fill_manual(values = c("#754a7e","#2197A9")) +
   #scale_colour_brewer(palette = "Set2") +
   #scale_fill_brewer(palette = "Set2") +
-  geom_bar(colour = "#58595b", stat = "identity") +
+  geom_bar(colour = "#ffffff", stat = "identity") +
   facet_grid(.~Position_f)+
-  xlab("Gender") +
-  ylab("Faculty Members") +
+  xlab("") +
+  ylab("Number of Faculty") +
   ggtitle("All Faculty by Position at Tennessee-Knoxville") +
   theme(
     axis.text.y = element_text(color="#58595B", size = 20, vjust = 0.5),
@@ -130,10 +165,12 @@ facultyaggregatebypositionplot <- ggplot(data = facultybyposition, aes(x = Gende
     plot.title = element_text(color="#58595B", face = "bold", size = 20, hjust = 0.5, vjust = 0),
     panel.background = element_blank(),
     #panel.border = element_rect(color="#58595B"),#element_blank(),
-    panel.grid.major.x = element_line(color = "#A7A9AC", size = 0.5),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
     panel.grid.major.y = element_line(color = "#A7A9AC", size = 0.5),
+    panel.grid.minor.y = element_blank(),
     legend.position = "right"
   )
 
 facultyaggregatebypositionplot
-ggsave(filename = "plots/facultyaggregatebypositionplot.svg", plot = facultyaggregatebypositionplot, width = 14, height = 6, units = "in")
+ggsave(filename = "plots/facultyaggregatebypositionplot.svg", plot = facultyaggregatebypositionplot, width = 10, height = 6, units = "in")
--- transform-data.R
+++ transform-data.R
@@ -2,6 +2,7 @@ library(plyr)
 
 ### stem men women by department ----
 stemfaculty = utkfacultydata[utkfacultydata$Stem =="stem",]
+stemfacultytenure <- stemfaculty[grep("Prof", stemfaculty$Position),]
 stemfacultynums <- count(stemfaculty, c("Department", "Gender"))
 #stemfacultynums$Department <- with(stemfacultynums,factor(Department, levels = Department[order(stemfacultynums$freq)]))
 # deprecated: stemfacultynums_ordered <- with(stemfacultynums, stemfacultynums[order(-freq, Gender),])
@@ -10,22 +11,42 @@ stemfacultynums <- count(stemfaculty, c("Department", "Gender"))
 nonstemfaculty = utkfacultydata[utkfacultydata$Stem =="notstem",]
 stemfacultybygender <- count(stemfaculty, c("Gender"))
 nonstemfacultybygender <- count(nonstemfaculty, c("Gender"))
-facultybygender = rbind(stemfacultybygender,nonstemfacultybygender)
+facultybygender = rbind(stemfaultybygender,nonstemfacultybygender)
 facultybygender = cbind(facultybygender, Designation = c("stem","stem","non-stem","non-stem"))
+
+### men women non-stem stem aggregate ----
+nonstemfaculty = utkfacultydata[utkfacultydata$Stem =="notstem",]
+nonstemfacultytenure <- nonstemfaculty[grep("Prof", stemfaculty$Position),]
+stemfacultytenurebygender <- count(stemfacultytenure, c("Gender"))
+nonstemfacultytenurebygender <- count(nonstemfacultytenure, c("Gender"))
+facultytenurebygender = rbind(stemfacultytenurebygender,nonstemfacultytenurebygender)
+facultytenurebygender = cbind(facultytenurebygender, Designation = c("stem","stem","non-stem","non-stem"))
+
 
 ### men women non-stem stem by position ----
 stemfacultybyposition <- count(stemfaculty, c("Position", "Gender"))
 
-sumntfem = sum(stemfacultybyposition[stemfacultybyposition$Position=="Instructor" & stemfacultybyposition$Gender=="Female",3],
-      stemfacultybyposition[stemfacultybyposition$Position=="Lecturer" & stemfacultybyposition$Gender=="Female",3],
-      stemfacultybyposition[stemfacultybyposition$Position=="Other" & stemfacultybyposition$Gender=="Female",3])
-sumntmal = sum(stemfacultybyposition[stemfacultybyposition$Position=="Instructor" & stemfacultybyposition$Gender=="Male",3],
-               stemfacultybyposition[stemfacultybyposition$Position=="Lecturer" & stemfacultybyposition$Gender=="Male",3],
-               stemfacultybyposition[stemfacultybyposition$Position=="Other" & stemfacultybyposition$Gender=="Male",3])
+sumstemntfem = sum(stemfacultybyposition[!grepl("Prof", stemfacultybyposition$Position) & stemfacultybyposition$Gender=="Female",3])
+sumstemntmal = sum(stemfacultybyposition[!grepl("Prof", stemfacultybyposition$Position) & stemfacultybyposition$Gender=="Male",3])
 
-stemfacultynontenure  <- data.frame(matrix(data = c("Position"="Non-Tenure", "Gender" = "Female", freq= sumntfem,
-                                    "Position"="Non-Tenure", "Gender" = "Male", freq= sumntmal), nrow = 2, byrow = TRUE))
+  
+stemfacultynontenure  <- data.frame(matrix(data = c("Non-Tenure", "Female", sumstemntfem,
+                                    "Non-Tenure", "Male", sumstemntmal), nrow = 2, byrow = TRUE), stringsAsFactors = FALSE)
+colnames(stemfacultynontenure) <- c("Position", "Gender", "freq")
+stemfacultynontenure$freq <- as.numeric(stemfacultynontenure$freq)
+
 
 nonstemfacultybyposition <- count(nonstemfaculty, c("Position", "Gender"))
-facultybyposition = rbind(stemfacultybyposition,nonstemfacultybyposition)
-facultybyposition = cbind(facultybyposition, Designation = c(rep("stem", times = 12),rep("non-stem",12)))
+
+sumnonstemntfem = sum(nonstemfacultybyposition[!grepl("Prof", nonstemfacultybyposition$Position) & nonstemfacultybyposition$Gender=="Female",3])
+sumnonstemntmal = sum(nonstemfacultybyposition[!grepl("Prof", nonstemfacultybyposition$Position) & nonstemfacultybyposition$Gender=="Male",3])
+
+
+nonstemfacultynontenure  <- data.frame(matrix(data = c("Non-Tenure", "Female", sumnonstemntfem,
+                                                    "Non-Tenure","Male", sumnonstemntmal), nrow = 2, byrow = TRUE),stringsAsFactors = FALSE)
+colnames(nonstemfacultynontenure) <- c("Position", "Gender", "freq")
+nonstemfacultynontenure$freq <- as.numeric(nonstemfacultynontenure$freq)
+
+facultybyposition = rbind(stemfacultybyposition[grepl("Prof", stemfacultybyposition$Position),],stemfacultynontenure,nonstemfacultybyposition[grepl("Prof", nonstemfacultybyposition$Position),],nonstemfacultynontenure)
+facultybyposition = cbind(facultybyposition, Designation = c(rep("stem", times = 8),rep("non-stem",8)))
+row.names(facultybyposition)<-seq.int(1,16,1)
